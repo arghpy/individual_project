c1318c3dc95b36f240e14d6c1f4e184c110136ea
arghpy <subaandrei2@gmail.com>
2022-11-30 12:28:50 +0200

    work on script

--- installation_script.sh
+++ installation_script.sh
@@ -1,3 +1,30 @@
 #!/usr/bin/env bash
 
+# Installation script
+SCRIPT="https://raw.githubusercontent.com/arghpy/suckless_progs/main/installation_script.sh"
 
+# Programs to install
+PROGS_GIT="https://raw.githubusercontent.com/arghpy/suckless_progs/main/packages.csv"
+
+
+
+# Check for internet
+check_internet() {
+
+	if [[ $(ping -c1 8.8.8.8 > /dev/null 2>&1 ; echo $?) != 0 ]]; then
+
+		whiptail --title "Internet connection"\
+			 --yes-button "nmtui"
+			 -no-button "Retry"
+			 --msgbox "Please check your internet connection.\\nIf your connection is through ethernet then make sure that the cable is connected.\\nIf you wish to connect thorugh wifi select 'nmtui'. Other wise select 'retry'." 
+
+		case "$(echo $?)" in
+			1) nmtui ;;
+			0) check_internet ;;
+			*) exit 1 ;;
+		esac
+	
+	fi
+}
+
+check_internet
